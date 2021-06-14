7d487afad97545ceffbdfae4310c61f1408d49a2
izidormaklary <maklaryizidor@gmail.com>
2021-06-14 16:31:22 +0200

    database connection

---
+++ Model/connection.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+function openConnection(): PDO
+{
+
+    $dbhost = "localhost";
+    $dbuser = "becode";
+    $dbpass = "becode";
+    $db = "Products";
+
+    $driverOptions = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+
+    return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
+}
+
+$pdo = openConnection();
